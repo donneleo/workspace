8aeab7aff33e0b084c3d53711bad0eb1733e334b
donneleo <60257358+donneleo@users.noreply.github.com>
2020-02-25 15:23:19 +0000

    Add files via upload
    updated and fixed

--- SortComparisonTest.java
+++ SortComparisonTest.java
@@ -33,7 +33,73 @@ import java.io.*;
  *  
  *  e)The insertion sort was the quickest for the 10 element data set, but for each data set thereafter, the recursive merge sort
  *  was the quickest
+ *  
+ *    /**
+     *  Main Method.
+     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     *
+     */
+    
+	/*public static void main(String[] args) throws FileNotFoundException
+	{
+		double[] a = new double[1000];     
+		FileReader input = (new FileReader("numbersSorted1000.txt"));
+		Scanner scanner = new Scanner(input);
+		for(int i=0; i<a.length; i++) 
+		{
+			a[i] = scanner.nextDouble();
+		}
+		scanner.close();
+
+
+		long startTime = System.nanoTime();
+		SortComparison.insertionSort(a);
+		long endTime = System.nanoTime();
+		System.out.println(((endTime-startTime)));
+
+		startTime = System.nanoTime();
+		SortComparison.selectionSort(a);
+		endTime = System.nanoTime();
+		System.out.println(((endTime-startTime)));
+
+		startTime = System.nanoTime();
+		SortComparison.quickSort(a);
+		endTime = System.nanoTime();
+		System.out.println(((endTime-startTime)));
+
+		startTime = System.nanoTime();
+		SortComparison.mergeSortIterative(a);
+		endTime = System.nanoTime();
+		System.out.println(((endTime-startTime)));
+
+		startTime = System.nanoTime();
+		SortComparison.mergeSortRecursive(a);
+		endTime = System.nanoTime();
+		System.out.println(((endTime-startTime)));
+	}
  *
+ *
+ *  //-----------Run Time Averages---------------
+ *  
+ *  
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |              | Insertion Sort | Selection Sort | MergeSort Iterative | MergeSort Recursive | QuickSort |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |numbers10.txt |   9833.33ns    |   12500ns      |     59033.33ns      |     35766.66ns      | 29566.6ns |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |numbers100.txt|   256200ns     |   83733.33ns   |     61900ns         |        40100ns      |  149000ns |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |numbers1000   |    8.95ms      |    3.83ms      |       .48ms         |         .25ms       |  4.69ms   |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |numbers1000   |                |                |                     |                     |           |
+ *  | few unique   |    8.47ms      |    3.48ms      |       .49ms         |         .24ms       |  1.63ms   |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |1000 near ord |    8.02ms      |    5.17ms      |       .51ms         |         .23ms       |  4.82ms   |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |1000 reversed |    8.82ms      |    3.37ms      |       .64ms         |         .23ms       |  3.93ms   |
+ *  ----------------------------------------------------------------------------------------------------------
+ *  |1000 sorted   |    5.49ms      |    3.93ms      |       .56ms         |         .21ms       |  4.53ms   |
+ *  ----------------------------------------------------------------------------------------------------------
  */
 @RunWith(JUnit4.class)
 public class SortComparisonTest
@@ -106,35 +172,6 @@ public class SortComparisonTest
     }
 
     // ----------------------------------------------------------
-    /**
-     *  Main Method.
-     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
-     *
-     */
-    
-    public void main(String[] args) throws FileNotFoundException
-    {
-        double[] a = new double[10];
-        int i=0;
-        
-        FileReader input = (new FileReader("numbers1000.txt"));
-        Scanner scanner = new Scanner(input);
-        while(scanner.hasNextLine()) 
-        {
-        	a[i] = scanner.nextDouble();
-        	i++;
-        }
-        scanner.close();
-        
-        for(int j=0; j<a.length; j++) 
-        {
-        	System.out.println(a[j]);
-        }
-        
-        long startTime = System.currentTimeMillis();
-        SortComparison.insertionSort(a);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Insertion sort with 1000 elements took " + (endTime-startTime) + " ms.");
-    }
+  
 
 }
